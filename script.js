6dac09000126de7c7671800c2b791f2c64bf31a3
Aya Hssaini <ayaguendouf1@gmail.com>
2026-07-09 12:41:15 +0200

    fix: ajustement graphique final

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const montant = document.getElementById("montant");
 const resultat = document.getElementById("resultat");
 
-montant.addEventListener("input", () => {
-    const valeur = parseFloat(montant.value);
+montant.addEventListener("input", function () {
+    const note = parseFloat(montant.value);
 
-    if (!isNaN(valeur)) {
-        const pourboire = valeur * 0.15;
-        resultat.textContent = "Pourboire : " + pourboire.toFixed(2) + " DH";
-    } else {
+    if (isNaN(note) || note < 0) {
         resultat.textContent = "";
+        return;
     }
+
+    const pourboire = note * 0.20; // 20%
+    resultat.textContent = "Pourboire : " + pourboire.toFixed(2) + " DH";
 });
